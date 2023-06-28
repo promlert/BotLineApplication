7a71f2ca4c09df883519f53be73390ed7e9d91c3
promlert <Promlert_Pearrukkrai@hotmail.com>
2023-06-28 22:02:56 +0700

    fix db

--- BotLineApplication/DB/script.sql
+++ BotLineApplication/DB/script.sql
@@ -1,27 +1,38 @@
 USE [LineDB]
 GO
-
-/****** Object:  Table [dbo].[SourceState]    Script Date: 6/19/2023 12:51:57 AM ******/
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[SourceState]') AND type in (N'U'))
-DROP TABLE [dbo].[SourceState]
-GO
-
-/****** Object:  Table [dbo].[SourceState]    Script Date: 6/19/2023 12:51:57 AM ******/
+/****** Object:  Table [dbo].[LogMessage]    Script Date: 6/28/2023 10:02:18 PM ******/
 SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
-
+CREATE TABLE [dbo].[LogMessage](
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[UserId] [varchar](50) NOT NULL,
+	[Text] [varchar](300) NULL,
+	[CreateDate] [datetime] NULL,
+ CONSTRAINT [PK_LogMessage] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[SourceState]    Script Date: 6/28/2023 10:02:18 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 CREATE TABLE [dbo].[SourceState](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[UserName] [varchar](50) NULL,
+	[UserName] [varchar](50) NOT NULL,
 	[DisplayName] [varchar](50) NULL,
+	[GroupName] [varchar](50) NULL,
+	[Room] [varchar](50) NULL,
 	[SourceType] [varchar](50) NULL,
+	[Account] [varchar](50) NULL,
+	[VehicleRegistration] [varchar](50) NULL,
 	[CreateDate] [datetime] NULL,
  CONSTRAINT [PK_UserLine] PRIMARY KEY CLUSTERED 
 (
-	[Id] ASC
+	[UserName] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
